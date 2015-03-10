9dce01009e9ed09f2341bbdaf7b775543d9281ba
Thomas LaFreniere <thomas@lafreniere.co>
2015-03-09 18:51:02 -0700

    Client pretty and client controllable username

--- public/javascripts/vhs.js
+++ public/javascripts/vhs.js
@@ -2,17 +2,53 @@
 var socket = io();
 
 $(function() {
+    ensureUsername();
+
+    $('#username').change(function(asdf) {
+        ensureUsername();
+    });
+
     $('form').submit(function() {
+        ensureUsername();
+
         socket.emit("web-message", {
             text: $('#m').val(),
-            from: "Web User"
+            from: $('#username').val()
         });
         $('#m').val('');
         return false;
     });
 
     socket.on('slack-message', function(msg){
-        $('#messages').append($('<li>').text(msg.username + ": " + msg.text));
+        var now = new Date();
+        $('#messages').append($('<li>').html("<b>" + msg.username + "</b> <span class=\"time\">"+ now.toTimeString() +"</span><br />" + msg.text));
+        window.scrollTo(0,document.body.scrollHeight);
     });
 });
 
+function ensureUsername() {
+    var name = $('#username').val();
+
+    if (!name || /^\s*$/.test(name)) {
+        name = localStorage.getItem("username");
+
+        if (!name || /^\s*$/.test(name)) {
+            $.ajax({
+                url: 'http://api.randomuser.me/',
+                dataType: 'json',
+                success: function (data) {
+                    setUsername(data.results[0].user.username);
+                }
+            });
+        } else {
+            setUsername(name);
+        }
+    } else {
+        setUsername(name);
+    }
+}
+
+function setUsername(username) {
+    localStorage.setItem("username", username);
+    $('#username').val(username);
+}
